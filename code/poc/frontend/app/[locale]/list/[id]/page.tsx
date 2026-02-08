8833b9a518c315f0d4bf577a67509067acff7633
omeon333 <omeoncoin@gmail.com>
2026-02-08 19:16:05 +0000

    fix: add pb-24 to list detail page for mobile nav clearance

--- code/poc/frontend/app/[locale]/list/[id]/page.tsx
+++ code/poc/frontend/app/[locale]/list/[id]/page.tsx
@@ -533,7 +533,7 @@ export default function CuratedListDetailPage() {
         </div>
 
         {/* Main Content */}
-        <div className="max-w-4xl mx-auto px-4 py-6">
+        <div className="max-w-4xl mx-auto px-4 pt-6 pb-24">
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
